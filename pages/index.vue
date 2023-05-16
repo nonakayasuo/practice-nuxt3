a84777bb96a575f056bc324142a6a742cfef55da
ahemoe <gr0265xk@gmail.com>
2023-05-16 15:31:49 +0900

    feat: add ability to call chat gpt api

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const keyword = ref("");
+
+const handleClick = () => {
+  // ここに生成する処理をかく
+};
+</script>
+
+<template>
+  <h1>GPT-3 APP</h1>
+  <div>
+    <input type="text" v-model="keyword" />
+  </div>
+  <div>
+    <button type="button" @click="handleClick">テキスト生成</button>
+  </div>
+</template>
