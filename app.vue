48c55e1feec5280161d269094798f212801b0b3a
nonakayasuo <gr0265xk@gmail.com>
2023-11-11 19:20:37 +0900

    Update: リスト2-22まで

--- app.vue
+++ app.vue
@@ -1,15 +1,9 @@
-<script setup lang="ts">
-const msg = shallowRef("まだ");
-const onButtonClick = (label: string, event: Event): void => {
-  const target = event.target as HTMLButtonElement;
-  const text = target.innerText;
-  msg.value = `${label}と${text}`;
-};
-</script>
-
+<script lang="ts" setup></script>
 <template>
-  <p>
-    {{ msg }}
-  </p>
-  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
+  <section class="border border-blue-500 m-2.5 p-4">
+    <h1 class="font-bold text-lg">コンポーネント基礎</h1>
+    <OneSection />
+    <OneSection />
+    <PartsTheSupplement />
+  </section>
 </template>
