3ff26354fbbe28fb7dcd1a56a4fc836090a5c3cd
nonakayasuo <gr0265xk@gmail.com>
2023-11-11 18:19:13 +0900

    Update: リスト2-1

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+const widthInit = Math.round(Math.random() * 10);
+const heightInit = Math.round(Math.random() * 10);
+const width = ref(widthInit);
+const height = ref(heightInit);
+const area = computed((): number => {
+  return width.value * height.value;
+});
+setInterval(
+  (): void => {
+    width.value = Math.round(Math.random() * 10);
+    height.value = Math.round(Math.random() * 10);
+  },
+  1000,
+  true
+);
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <p>縦{{ height }}で横が{{ width }}の長方形の面積は{{ area }}</p>
 </template>
